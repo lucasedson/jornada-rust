9a01fcd5ff18f8b1e46d7c5d7ffe23026d1a7fc5
Lucas Edson <lucasedson654@gmail.com>
2023-08-07 20:02:58 -0300

    Exercicio 01 - Funções Ok

--- 01 - Nivel Inicial/01 - Introdução e Sintaxe Basica/exercicios-funcoes/exercicio-01/src/main.rs
+++ 01 - Nivel Inicial/01 - Introdução e Sintaxe Basica/exercicios-funcoes/exercicio-01/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    fn calc(a: f64, b: f64, op: &str) -> f64 {
+        if op == "plus" {
+            return a + b;
+        } else if op == "multiply" {
+            return a * b;
+        } else if op == "minus" {
+            return a - b;
+        } else {
+            return a / b;
+        }
+    }
+    println!("{}", calc(5.0, 4.0, "plus"));
+    println!("{}", calc(5.0, 4.0, "multiply"));
+    println!("{}", calc(5.0, 4.0, "minus"));
+    println!("{}", calc(5.0, 4.0, "divide"));
 }
