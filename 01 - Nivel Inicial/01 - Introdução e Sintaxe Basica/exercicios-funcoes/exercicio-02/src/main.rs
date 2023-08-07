9257c0a60695052ffb7242ecec4c56a12415c424
Lucas Edson <lucasedson654@gmail.com>
2023-08-07 20:07:18 -0300

    Exercicio 02 - Funções Ok

--- 01 - Nivel Inicial/01 - Introdução e Sintaxe Basica/exercicios-funcoes/exercicio-02/src/main.rs
+++ 01 - Nivel Inicial/01 - Introdução e Sintaxe Basica/exercicios-funcoes/exercicio-02/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    fn verificar_paridade(number: i32) -> String {
+        if number % 2 == 0 {
+            return "Par".to_string();
+        } else {
+            return "Impar".to_string();
+        }
+    }
+
+    println!("{}", verificar_paridade(13));
+    println!("{}", verificar_paridade(12));
 }
