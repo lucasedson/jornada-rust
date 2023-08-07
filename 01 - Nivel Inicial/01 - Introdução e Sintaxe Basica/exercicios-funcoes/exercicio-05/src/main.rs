68d9aa50d3424dd84e5f4d5e3c677dbe6c029262
Lucas Edson <lucasedson654@gmail.com>
2023-08-07 20:48:19 -0300

    Exercicio 05  Fatorial- Funções Ok

--- 01 - Nivel Inicial/01 - Introdução e Sintaxe Basica/exercicios-funcoes/exercicio-05/src/main.rs
+++ 01 - Nivel Inicial/01 - Introdução e Sintaxe Basica/exercicios-funcoes/exercicio-05/src/main.rs
@@ -1,3 +1,13 @@
+//Fatorial: Escreva uma função fatorial que calcula o fatorial de um número inteiro positivo usando recursão.
+//NOTA: Não usei recursão pq não é uma boa Prática
 fn main() {
-    println!("Hello, world!");
+    fn fatorial(numero: i128) -> i128 {
+        let mut acc: i128 = numero;
+        for i in (1..(numero)).rev() {
+            acc = acc * i;
+        }
+        return acc;
+    }
+    let num: i128 = 5;
+    println!("O Fatorial de {} é: {}", num, fatorial(num));
 }
