efabbe15c4427b5c69a188519c92ff2a5875d0a6
Lucas Edson <lucasedson654@gmail.com>
2023-08-07 20:18:57 -0300

    Exercicio 03 - Funções Ok

--- 01 - Nivel Inicial/01 - Introdução e Sintaxe Basica/exercicios-funcoes/exercicio-03/src/main.rs
+++ 01 - Nivel Inicial/01 - Introdução e Sintaxe Basica/exercicios-funcoes/exercicio-03/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    fn calcular_media(lista: Vec<i32>) -> f32 {
+        let mut sum: i32 = 0;
+        for i in &lista {
+            sum += i;
+        }
+        return (sum / lista.len() as i32) as f32;
+    }
+
+    let lista = vec![0, 1, 2, 3, 4, 5, 6];
+    let res = calcular_media(lista);
+    println!("A média de lista é: {}", res)
 }
