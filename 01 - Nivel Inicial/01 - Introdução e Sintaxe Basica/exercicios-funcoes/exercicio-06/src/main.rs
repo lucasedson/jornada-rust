b4e052a1f750603307ee14b672597387c0ac4503
lucasedson <lucasedson654@gmail.com>
2023-09-23 19:12:13 -0300

    Exercicio 06 Ok

--- 01 - Nivel Inicial/01 - Introdução e Sintaxe Basica/exercicios-funcoes/exercicio-06/src/main.rs
+++ 01 - Nivel Inicial/01 - Introdução e Sintaxe Basica/exercicios-funcoes/exercicio-06/src/main.rs
@@ -1,3 +1,17 @@
+// Soma de Números Pares: Implemente uma função soma_pares que recebe
+// um número inteiro positivo n como parâmetro
+//  e retorna a soma dos primeiros n números pares.
+
+fn soma_pares(n: i32) -> i32 {
+    let mut acc: i32 = 0;
+    for i in 0..(n + 1) {
+        if i % 2 == 0 {
+            acc = acc + i;
+        }
+    }
+    return acc;
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", soma_pares(10000));
 }
