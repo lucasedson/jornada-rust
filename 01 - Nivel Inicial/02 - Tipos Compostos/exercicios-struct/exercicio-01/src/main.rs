4f544b99e89f9060c1209b7e6b19450c6e955c79
lucasedson <lucasedson654@gmail.com>
2023-09-23 19:29:01 -0300

    Exercicio Ok

--- 01 - Nivel Inicial/02 - Tipos Compostos/exercicios-struct/exercicio-01/src/main.rs
+++ 01 - Nivel Inicial/02 - Tipos Compostos/exercicios-struct/exercicio-01/src/main.rs
@@ -1,3 +1,29 @@
+// Exercício: Cadastro de Pessoas
+// Crie uma struct chamada Pessoa que represente uma pessoa com campos como nome, idade, cidade e e-mail.
+// Escreva um programa que crie várias instâncias da struct e imprima as informações de cada pessoa.
+
+struct Pessoa {
+    nome: String,
+    idade: i16,
+    cidade: String,
+    email: String,
+}
+
+fn nova_pessoa(pessoa: Pessoa) -> Pessoa {
+    println!(
+        "Nome: {}\nIdade: {}\nCidade: {}\nEmail: {}",
+        pessoa.nome, pessoa.idade, pessoa.cidade, pessoa.email
+    );
+    return pessoa;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let pessoa_1 = Pessoa {
+        nome: String::from("Lucas"),
+        idade: 27,
+        cidade: String::from("Rio de Janeiro"),
+        email: String::from("email@email.com"),
+    };
+
+    nova_pessoa(pessoa_1);
 }
