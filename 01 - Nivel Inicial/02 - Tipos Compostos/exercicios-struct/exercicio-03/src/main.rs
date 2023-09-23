32c1cf4bc683cc3bb3cacb883ee68fdbdc6bc9a5
lucasedson <lucasedson654@gmail.com>
2023-09-23 20:05:36 -0300

    Exercicio 03 Struct Ok

--- 01 - Nivel Inicial/02 - Tipos Compostos/exercicios-struct/exercicio-03/src/main.rs
+++ 01 - Nivel Inicial/02 - Tipos Compostos/exercicios-struct/exercicio-03/src/main.rs
@@ -1,3 +1,49 @@
+// Crie uma struct chamada ContaBancaria com campos para armazenar o nome do titular, saldo e número da conta.
+// Implemente métodos para depositar e sacar dinheiro da conta, atualizando o saldo corretamente.
+// Escreva um programa que demonstre o funcionamento da struct e seus métodos.
+
+struct ContaBancaria {
+    nome: String,
+    saldo: i64,
+    numero_conta: i32,
+}
+
+impl ContaBancaria {
+    fn depositar(&mut self, valor: i64) {
+        self.saldo += valor;
+        println!("Saldo Atualizado!")
+    }
+
+    fn sacar(&mut self, valor: i64) {
+        if self.saldo >= (self.saldo - valor) {
+            self.saldo -= valor;
+            println!(
+                "Você sacou: {}, Saldo Atualizado!, Saldo atual: {}",
+                valor, self.saldo
+            );
+        } else {
+            println!("Saldo insufiente");
+        }
+    }
+    fn mostrar_informacoes(&self) {
+        println!(
+            "Nome: {}, Saldo: {}, Número da conta: {}",
+            self.nome, self.saldo, self.numero_conta
+        )
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut conta_1 = ContaBancaria {
+        nome: String::from("Lucas"),
+        saldo: 1000,
+        numero_conta: 0001,
+    };
+
+    conta_1.mostrar_informacoes();
+    conta_1.depositar(2000);
+
+    conta_1.mostrar_informacoes();
+
+    conta_1.sacar(500);
 }
